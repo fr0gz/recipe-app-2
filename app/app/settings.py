eec9e16de567198d31ca822eb48b98509275d93c
fr0gz <cguevarak@fen.uchile.cl>
2020-12-14 18:44:05 -0300

    le meti un endpoint para gestionar usuarios, con un token de auth

--- app/app/settings.py
+++ app/app/settings.py
@@ -38,8 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #Third party apps
+    'rest_framework',
+    'rest_framework.authtoken',
     #Local
     'core',
+    'user'
 ]
 
 MIDDLEWARE = [
